33f3bf1a0996955f8d45f6cf17d209b71b81c6d0
kacpersoltys2 <126696092+kacpersoltys@users.noreply.github.com>
2023-03-29 14:22:30 +0000

    for(int j=0; j<wysokosc-i; j++) na  for(int j=0; j<wysokosc-i-1; j++)

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ System.out.print("Podaj wartość dla wysokości choinki: ");
   int wysokosc = skaner.nextInt();
 
   for(int i=0; i<wysokosc; i++) {
-    for(int j=0; j<wysokosc-i; j++) {
+    for(int j=0; j<wysokosc-i-1; j++) {
       System.out.print(" ");
     }
 for (int k=0; k<=i; k++) {
