7f0dde22bbd0480bf90feae215879ef80574693d
kacpersoltys2 <126696092+kacpersoltys@users.noreply.github.com>
2023-03-29 14:12:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+public static void main(String[]args) {
+  Scanner skaner = new Scanner(System.in);
+System.out.print("Podaj wartość dla wysokości choinki: ");
+  int wysokosc = skaner.nextInt();
+
+  for(int i=0; i<wysokosc; i++) {
+    for(int j=0; j<wysokosc-i; j++) {
+      System.out.print(" ");
+    }
+for (int k=0; k<=i; k++) {
+  System.out.print("*");
+}    
+    System.out.println();
+  }
+  for(int i=0; i<wysokosc; i++) {
+    for(int j=0; j>wysokosc+i; j++) {
+      System.out.print(" ");
+    }
+for (int k=0; k<=i; k++) {
+  System.out.print("*");
+}    
+    System.out.println();
+  }
+}
+}
